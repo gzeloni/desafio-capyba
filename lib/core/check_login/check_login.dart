51f0beffa4e21fb306382f3a59a35cdd0dfd39bf
Gustavo Zeloni <gustavoluizdejesus@gmail.com>
2023-03-24 14:32:04 -0300

    documentation for CheckLogin class

--- lib/core/check_login/check_login.dart
+++ lib/core/check_login/check_login.dart
@@ -1,7 +1,10 @@
+// package do Flutter
+import 'package:flutter/material.dart';
+// Dependência do Firebase Auth
+import 'package:firebase_auth/firebase_auth.dart';
+// Telas: Login e Navbar (explicada mais abaixo)
 import 'package:desafio_capyba/src/navbar/navbar.dart';
 import 'package:desafio_capyba/src/screens/login_page.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 
 class CheckLogin extends StatelessWidget {
   const CheckLogin({super.key});
@@ -13,6 +16,12 @@ class CheckLogin extends StatelessWidget {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
+            /**
+             * A NavBar compõe a tela e mantém uma barra de navegação
+             * na base da tela.
+             * Retornar a NavBar, retorna diretamente a Home e também
+             * a área restrita do app.
+            */
             return const NavBar();
           } else {
             return const LoginPage();
