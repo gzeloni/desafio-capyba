0d772eabeabc69f160db550c43ae1b6704f935aa
Gustavo Zeloni <gustavoluizdejesus@gmail.com>
2023-03-24 10:08:15 -0300

    now camera don't start in the main

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,8 @@ import 'package:flutter/material.dart';
 // Dependências do Firebase
 import 'package:firebase_core/firebase_core.dart';
 import 'package:desafio_capyba/core/firebase/firebase_options.dart';
-// Dependência da câmera
-import 'package:camera/camera.dart';
 // Tela CheckLogin (vide core/check_login)
 import 'package:desafio_capyba/core/check_login/check_login.dart';
-
-/*
- * Essa variável é global no projeto para que seja possível
- * Ler a lista de câmeras disponíveis em qualquer tela.
-*/
-// late CameraDescription firstCamera;
 
 Future<void> main() async {
   // Certifique-se de que os serviços de plug-in sejam inicializados para que `availableCameras()`
